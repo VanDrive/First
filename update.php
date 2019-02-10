06f3fb5c639d6f1b87328ac05b67173e5c67b16b
van_dan <vandan.ua@gmail.com>
2019-02-10 11:22:10 +0200

    update corrected

--- update.php
+++ update.php
@@ -1,4 +1,5 @@
 <?php
+$id = $_POST['id'];
 $name = $_POST['name'];
 $description = $_POST['description'];
 $created_at = $_POST['created_at'];
@@ -12,17 +13,33 @@ $db_table = 'article';
 $conn_string = "pgsql:host=$db_host;port=5432;dbname=$db_name;user=$db_user;password=$db_password";
 $mysqli = new PDO($conn_string);
 
-if( isset($name) ) {
-    $stmt = $mysqli->prepare("UPDATE article SET description=:description, created_at=:created_at WHERE name=:name");
-    $stmt->bindValue('name', $name);
-    $stmt->bindValue('created_at', $created_at);
-    $stmt->bindValue('description', $description);
-    $stmt->execute();
-}
+$stmt = $mysqli->prepare("SELECT id ,name, description, created_at FROM article");
+$stmt->execute();
+$result = $stmt->fetchAll();
 
+echo "<table border='1'>";
+foreach ($result as $val) {
+    echo '<tr>';
+    echo '<td>'.$val['id'].'</td>';
+    echo '<td>'.$val['name'].'</td>';
+    echo '<td>'.$val['description'].'</td>';
+    echo '<td>'.$val['created_at'].'</td>';
+    echo '</tr>';
+}
+echo '</table>';
+
+if($id != NULL) {
+        $stmt = $mysqli->prepare("UPDATE article SET name=:name, description=:description, created_at=:created_at WHERE id=:id");
+        $stmt->bindValue('id', $id);
+        $stmt->bindValue('name', $name);
+        $stmt->bindValue('created_at', $created_at);
+        $stmt->bindValue('description', $description);
+        $stmt->execute();
+    }
 ?>
 
 <form action="update.php" method="post">
+    <p>ID</p><input type="text" name="id">
     <p>Name</p><input type="text" name="name">
     <p>Description</p><input type="text" name="description">
     <p>Created_at</p><input type="date" name="created_at">
